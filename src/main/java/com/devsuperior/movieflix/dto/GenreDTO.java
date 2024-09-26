cd23e4cb12c909c61dad7ffb74101e5f962231a7
icaro-freitas <icaro_freitas2014@outlook.com>
2024-09-26 12:10:32 -0300

    genres/ endpoint created

--- src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -1,23 +1,30 @@
 package com.devsuperior.movieflix.dto;
 
+import com.devsuperior.movieflix.entities.Genre;
+
 public class GenreDTO {
 
-    private Long id;
-    private String name;
+	private Long id;
+	private String name;
 
-    public Long getId() {
-        return id;
-    }
+	public GenreDTO(Genre entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
